68528df3892f1bd205f04a8b1c0dd661af572744
Maxy Marquez <maxy.marquez@hotmail.com>
2023-11-22 22:24:01 -0300

    -

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import ProtectedRoutes from './components/ProtectedRoutes/ProtectedRoutes'
 import UserPanel from './components/UserPanel/UserPanel'
 import Carrito from './components/Carrito/Carrito';
 import './App.css'
-axios.defaults.baseURL = 'https://backend-dev-jnpc.1.us-1.fl0.io//api'
+axios.defaults.baseURL = 'https://backend-dev-jnpc.1.us-1.fl0.io/api'
 
 
 
