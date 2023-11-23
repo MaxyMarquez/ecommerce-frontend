147edf15a7277ccae3b612eb53fdf00807a94cf7
Maxy Marquez <maxy.marquez@hotmail.com>
2023-11-23 12:59:31 -0300

    -

--- src/components/Detail/Details.jsx
+++ src/components/Detail/Details.jsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom';
 import styles from './Details.module.css';
 import NavBar from '../Navbar/NavBar';
-import Footer from '..Footer/Footer';
+import Footer from '../Footer/Footer';
 import Newsletter from '../Newsletter/Newsletter';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
